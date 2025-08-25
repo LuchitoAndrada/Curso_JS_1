0bf7fe7cfe517771cc0d0b8b4b03c20b7ebd03ca
Luchito <lucho@el.mejor>
2025-08-25 20:05:28 -0300

    Prueba

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
      console.log("Hello World desde el grupo 1") /* Imprime mensaje en consola */
-     alert("Mensaje Molesto") /* Imprime mensaje de alerta en la pantalla */
+     alert("Mensaje Molesto  ") /* Imprime mensaje de alerta en la pantalla */
