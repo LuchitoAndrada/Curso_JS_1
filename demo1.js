b920b7d1ddb93c8cdf8eb00fddf65b365bfb5271
Reychell <yo@quieroaprender.com>
2025-09-08 15:32:30 -0500

    Se modifica if del caso 1 y caso 3 para que aparezca directamente el error

--- demo1.js
+++ demo1.js
@@ -5,25 +5,35 @@ let opcion = parseInt(
   )
 );
 
-
 switch (opcion) {
   // Suma de dos números
   case 1:
     //declarar variable num1, convertir a entero el valor ingresado por el usuario
     let num1 = parseInt(prompt("Ingresa el primer número:"));
-    //declarar variable num2, convertir a entero el valor ingresado por el usuario
-    let num2 = parseInt(prompt("Ingresa el segundo número:"));
 
-    //si la variable num1 se marca como NaN en la consola o es menor o igual a 0 o la variable num2 es NaN o menor o igual a 0, entonces
-    if (isNaN(num1) || num1 <= 0 || isNaN(num2) || num2 <= 0) {
+    //si la variable num1 se marca como NaN en la consola o es menor o igual a 0, entonces
+    if (isNaN(num1) || num1 <= 0) {
       //le mostramos por consola y por alert al usuario que debe ingresar numeros validos
       console.log("Error: Debes ingresar números válidos mayores a 0.");
       alert("Error: Debes ingresar números válidos mayores a 0.");
-    } else {
-      //mostrar la suma en consola y en alerta
-      console.log(`La suma de ${num1} + ${num2} es: ${num1 + num2}`);
-      alert(`La suma es: ${num1 + num2}`);
+      break;
     }
+
+    //declarar variable num2, convertir a entero el valor ingresado por el usuario
+    let num2 = parseInt(prompt("Ingresa el segundo número:"));
+
+    //si la variable num2 es NaN o menor o igual a 0, entonces
+    if (isNaN(num2) || num2 <= 0) {
+      //le mostramos por consola y por alert al usuario que debe ingresar numeros validos
+      console.log("Error: Debes ingresar números válidos mayores a 0.");
+      alert("Error: Debes ingresar números válidos mayores a 0.");
+      break;
+    }
+
+    //mostrar la suma en consola y en alerta
+    console.log(`La suma de ${num1} + ${num2} es: ${num1 + num2}`);
+    alert(`La suma es: ${num1 + num2}`);
+
     break;
 
   // Cuenta regresiva desde un número mayor a 10
@@ -51,37 +61,37 @@ switch (opcion) {
   case 3:
     // Pedimos el nombre del usuario
     let nombre = prompt("Bienvenid@!\n¿Cuál es tu nombre?");
-    // Pedimos la edad del usuario
-    let edad = parseInt(prompt("¿Cuántos años tienes?"));
 
     // Validamos que el nombre no esté vacio ni tenga caracteres invalidos, solo los permitidos
-
     if (!nombre || !/^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\s]+$/.test(nombre.trim())) {
       console.log("Ingresa un nombre válido (solo letras y espacios).");
       alert("Ingresa un nombre válido (solo letras y espacios).");
       break;
-    } else if (
-      isNaN(edad) ||
-      edad <= 0 ||
-      edad > 99 ||
-      !Number.isInteger(edad)
-    ) {
+    }
+
+    // Pedimos la edad del usuario
+    let edad = parseInt(prompt("¿Cuántos años tienes?"));
+
+    //si la variable edad es NaN o menor o igual a 0 o mayor a 99 o si el numero iresado es diferente de tipo entero
+    if (isNaN(edad) || edad <= 0 || edad > 99 || !Number.isInteger(edad)) {
       //si edad is isNaN o edad menor o igual a 0 o edad mayor a 99 o edad diferente de valor entero, entonces
       console.log("Ingresa una edad válida (número entre 1 y 99).");
       alert("Ingresa una edad válida (número entre 1 y 99).");
       break;
-    } else {
-      // Mostramos el mensaje en consola y en alerta
-      console.log(
-        `¡Es un placer conocerte, ${nombre}! Tienes ${edad} años de edad.`
-      );
-      alert(`¡Es un placer conocerte, ${nombre}! Tienes ${edad} años de edad.`);
     }
 
-    break;
+    // Mostramos el mensaje en consola y en alerta
+    console.log(
+      `¡Es un placer conocerte, ${nombre}! Tienes ${edad} años de edad.`
+    );
+    alert(`¡Es un placer conocerte, ${nombre}! Tienes ${edad} años de edad.`);
 
-    default:
-        console.log("Opción inválida. Por favor, elige una opción entre 1 y 3. No ingreses letras o caracteres especiales.");
-        alert("Opción inválida. Por favor, elige una opción entre 1 y 3. No ingreses letras o caracteres especiales.");
+  default:
+    console.log(
+      "Opción inválida. Por favor, elige una opción entre 1 y 3. No ingreses letras o caracteres especiales."
+    );
+    alert(
+      "Opción inválida. Por favor, elige una opción entre 1 y 3. No ingreses letras o caracteres especiales."
+    );
     break;
 }
