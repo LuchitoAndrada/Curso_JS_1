32aa53c161613ac26268daab7355006d014c3fd1
Reychell <yo@quieroaprender.com>
2025-10-15 16:15:56 -0500

    se agrega boton home al navbar

--- js/navbar.js
+++ js/navbar.js
@@ -4,14 +4,18 @@ const categorias = ['Aretes', 'Collares', 'Anillos', 'Pulseras', 'Relojes'];
 // Variable para almacenar el menú
 let menu = "";
 
+// Agregamos primero el botón Home
+menu += `
+  <li class="nav-item">
+    <a class="nav-link px-3 home-link" href="./index.html">Home</a>
+  </li>`;
+
 // Creamos un bucle para generar cada enlace del menú
 for (let categoria of categorias) {
   menu += `
     <li class="nav-item">
       <a class="nav-link px-3 categoria-link" href="#" data-categoria="${categoria}">${categoria}</a>
     </li>`;
-  // "px-3" agrega espacio horizontal (padding left/right)
-  // "nav-link" aplica el estilo de enlace del navbar de Bootstrap
 }
 
 // Obtenemos el header del index
@@ -20,7 +24,6 @@ const header = document.querySelector("header");
 // Insertamos el navbar en el header
 header.innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm py-3">
-  <!-- py-3 agrega espacio vertical al navbar -->
   <div class="container">
     <a class="navbar-brand fw-bold" href="./index.html">Beauty You</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
